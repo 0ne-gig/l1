d9c51c45c1e2bf2d68a85fa4b1cc7d95778a3d16
ivan <ivan_25-rus@mail.ru>
2022-01-06 15:03:33 +1000

    deploy dev check

--- scripts/deploy_dev.sh
+++ scripts/deploy_dev.sh
@@ -1,8 +1,8 @@
 source ../.env/Scripts/activate
 cd ..
-pip list
 pip freeze > requirements.txt
 git add -A
 git commit -m "deploy dev check"
 git push origin master
-# ssh ivg
+cd scripts
+ssh ivg 'ls'
