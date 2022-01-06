6fbc4dbaa29ba6fd97d079b10ef9cc8a34bc188b
ivan <ivan_25-rus@mail.ru>
2022-01-06 15:17:46 +1000

    deplloy dev

--- scripts/deploy_dev.sh
+++ scripts/deploy_dev.sh
@@ -2,7 +2,7 @@ source ../.env/Scripts/activate
 cd ..
 pip freeze > requirements.txt
 git add -A
-git commit -m "deploy dev"
+git commit -m "deplloy dev"
 git push origin master
 cd scripts
 ssh doi 'cd lab1 && source scripts/build.sh'
